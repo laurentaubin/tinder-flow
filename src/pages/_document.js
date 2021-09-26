0f50d5077569a73cd525a7aa10582fcfdd9d098c
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2021-09-26 15:51:56 -0400

    Try adding social media preview

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,17 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 
-
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta property="og:url" content="https://chooseyourdate.netlify.app/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Choose your date !" />
+          <meta property="og:description" content="Make choices to find out what date you get" />
+          <meta property="og:image" content={"https://tinder.com/static/tinder.png"} />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
